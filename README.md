7bce7c761d034992112f15fcd2170f214c8a9e78
unknown <benclayton1@hotmail.com>
2021-08-13 15:52:40 +0100

    finished quiz

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # TheQuiz
+
+How I want this to look:
+
+1. User loads the loading screen and clicks to begin.
+2. The first question is randomly generated from the database and is displayed on the screen.
+3. The user types their answer into the text box which is then compared to the answer in the database.
+4. The user will be notified of the result and there will be a tally of how many they have gotten correct so far.
+5. At the end the user will be notified of their success rate and have the option to retake another quiz.
+
+Extended (if time)
+
+1. The user can login and have their results saved in a separate database table.
+2. A leaderboard can be displayed to see who is the best.
---
+++ app.js
@@ -0,0 +1,94 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+const questions = require('./questions.json')
+const session = require('express-session')
+
+const { Questions } = require('./models')
+
+let unique = []
+function checkUnique() {
+    if (unique.includes(random)){
+    random = Math.round(Math.random() * (42 - 1) + 1)
+    checkUnique()
+}
+    else return random;
+}
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    req.app.locals.correctAnswers = 0
+    req.app.locals.questionNumber = 0
+    unique = []
+    res.render('home.ejs')
+  })
+
+app.get('/firstQuestion', async (req, res) => {
+    const check = await Questions.findAll({where: {id: 1}})
+    if (check === false){
+    for(let i=0; i<41; i++){
+    await Questions.create({
+        question: questions.results[i].question,
+        answer: questions.results[i].correct_answer
+    })}}
+    
+    const random = Math.round(Math.random() * (42 - 1) + 1)
+    req.app.locals.question1 = await Questions.findAll({where: {id: random}})
+    unique.push(random)
+    req.app.locals.questionNumber += 1
+    res.render('questions.ejs', {
+        question: req.app.locals.question1,
+        number: req.app.locals.questionNumber
+    })
+})
+
+app.post('/result', async (req, res) => {
+    if(req.body.answer === req.app.locals.question1[0].answer){
+        req.app.locals.correctAnswers += 1
+        result= 'Correct'
+    }
+    else { 
+        result = 'Incorrect'
+    }
+
+    res.render('result.ejs', {
+        question: req.app.locals.question1,
+        result: result,
+        correct: req.app.locals.question1[0].answer,
+        tally: req.app.locals.correctAnswers,
+        number: req.app.locals.questionNumber
+    }) 
+})
+
+app.post('/questions', async (req, res) => {
+    if(req.app.locals.questionNumber >= 10){
+        res.render('finalScore.ejs',{
+            number: req.app.locals.questionNumber,
+            tally: req.app.locals.correctAnswers
+        })
+    }
+    else{    
+    random = Math.round(Math.random() * (42 - 1) + 1)
+    checkUnique()
+    req.app.locals.question1 = await Questions.findAll({where: {id: random}})
+    unique.push(random)
+    req.app.locals.questionNumber += 1
+    res.render('questions.ejs', {
+        question: req.app.locals.question1,
+        number: req.app.locals.questionNumber
+    })}
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
